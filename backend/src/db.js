0c0c6884aad862d9e6892c344a81139e376a7435
phoboPT <hugo.branco.c@gmail.com>
2019-09-18 16:53:21 +0100

    new

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,7 +2,7 @@
 const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
-    typeDefs: "/schema.graphql",
+    typeDefs: "src/generated/prisma.graphql",
     endpoint: process.env.PRISMA_ENDPOINT,
     secret: process.env.PRISMA_SECRET,
     debug: false,
